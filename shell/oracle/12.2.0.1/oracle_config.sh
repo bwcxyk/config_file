6a168a47f61c8d2cc4ae098d3b3a26f69dadaeb7
不忘初心 <1624717079@qq.com>
2019-10-25 11:06:41 +0800

    Update oracle_config.sh

--- shell/oracle/12.2.0.1/oracle_config.sh
+++ shell/oracle/12.2.0.1/oracle_config.sh
@@ -80,6 +80,7 @@ oracle           soft    nproc   2047
 oracle           hard    nproc   16384
 oracle           soft    nofile  1024
 oracle           hard    nofile  65536
+oracle           soft    stack   10240
 EOF
 echo "Step 4 : Successed~!"
 
