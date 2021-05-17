7c1c3085479a1ebc1b32a9cc8de53d8ca5a23d77
不忘初心 <1624717079@qq.com>
2021-05-17 09:05:24 +0800

    Update hosts_deny.sh

--- shell/hosts_deny.sh
+++ shell/hosts_deny.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
+# 把Failed 和Invalid超过5次的写到/etc/hosts.deny中
+
+#cat /var/log/secure|awk '/Invalid/{print $NF} /Failed/{print $(NF-3)}'|sort|uniq -c|awk '{print $2":"$1}' > blacklist
 cat /var/log/secure|awk '/Failed/{print $(NF-3)}'|sort|uniq -c|awk '{print $2":"$1}' > blacklist
 
 for line in `cat blacklist`;do
