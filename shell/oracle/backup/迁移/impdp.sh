a8e72793267e06d9a38efabad4db5d1718aaa712
不忘初心 <1624717079@qq.com>
2023-03-02 10:14:28 +0800

    Update impdp.sh

--- shell/oracle/backup/迁移/impdp.sh
+++ shell/oracle/backup/迁移/impdp.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #with impdp
-#set -e
-set -x
+#set -ex
+
+# shellcheck source=/dev/null
 source /home/oracle/.bash_profile
 dbuser="user"
-dbpasswd="pass"
-date=`date +'%Y%m%d'`
-files="bak_${date}0300*.dmp"
-dumpfile="bak_${date}0300_%U.dmp"
+dbpass="pass"
+date=$(date +'%Y%m%d')
+files="tmsbak_${date}0300*.dmp"
+file="tmsbak_${date}0300_%U.dmp"
 bakdir="/data/oracle/admin/orcl/dpdump"
 
-# 从备份机器复制数据文件
 function download() {
-    cd ${bakdir}
-    scp oracle@172.19.180.0:/data/oracle/admin/orcl/dpdump/${files} .
-    if [ $? -eq 0 ]; then
+    cd ${bakdir} || exit
+    if scp oracle@172.19.180.0:/data/oracle/admin/orcl/dpdump/"${files}" .;
+    then
         echo -e "\033[34;1m下载成功  \033[0m"
         echo
         echo -e "\033[33;1m文件存放路径：${bakdir} \033[0m" && ls ${bakdir}/
@@ -26,19 +26,23 @@ function download() {
 
 # 导入操作
 function data_import() {
-    impdp ${dbuser}/${dbpasswd} \
+    impdp "${dbuser}"/"${dbpass}" \
     DIRECTORY=DATA_PUMP_DIR \
     parallel=2 \
-    dumpfile=${dumpfile} \
-    remap_tablespace=tablespace1:tablespace2 \
-    remap_schema=user1:user2 \
+    dumpfile="${file}" \
+    remap_tablespace=tms_d_stand:tms_d_stand \
+    remap_schema=tms_user_sc:tms_temp_date_user \
+    exclude=SEQUENCE,OBJECT_GRANT,PACKAGE,FUNCTION,PROCEDURE,CONSTRAINT,VIEW,PACKAGE_BODY,TRIGGER,INDEX,PROCOBJ,JOB,DB_LINK \
+    exclude=table:\"like\ \'API%\'\" \
     table_exists_action=replace \
     transform=segment_attributes:n
+    echo $?
 }
 
 # 清理临时文件
 function delete() {
-    rm -f ${files}
+    echo "delete files"
+    rm -fv "${files}"
 }
 
 function main() {
