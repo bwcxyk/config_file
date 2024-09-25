2d782f5e7c54fc2bb765e9450aeb44cfdb4f6e7d
不忘初心 <1624717079@qq.com>
2024-09-25 15:21:24 +0800

    Update docker-entrypoint.sh

--- devops/node/docker-entrypoint.sh
+++ devops/node/docker-entrypoint.sh
@@ -6,7 +6,4 @@ if [ "$baseURL" ];
     sed -i "s|###baseURL###|$baseURL|g" /usr/share/nginx/html/*/js/app.*.js
 fi
 
-# use root
-sed -i "s|user  nginx;|user  root;|g" /etc/nginx/nginx.conf
-
-nginx -g 'daemon off;'
+exec "$@"
